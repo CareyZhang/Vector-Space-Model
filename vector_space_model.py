eb03c96e2b73daee6cc5005c39c9e87bd116720d
CareyZhang <loves29955173@gmail.com>
2019-10-02 03:22:34 +0800

    remove old version

--- vector_space_model.py
+++ vector_space_model.py
@@ -1,104 +1,95 @@
 import json
 import math
 
-def get_idf(doc_dataset,target,doc_cnt):
-	cnt = 0
-	for docs in doc_dataset:
-		if target in doc_dataset[docs].keys():
-			cnt += 1
-	return math.log(1+(doc_cnt/(1+cnt)),10)
-	
-def get_sim(query_tf_idf,doc_tf_idf):
-	return 0
+Query = [query.strip('\n') for query in open("query_list.txt","r")]
+Doc = [doc.strip('\n') for doc in open("doc_list.txt","r")]
 
-query_list = [query.strip('\n') for query in open("query_list.txt","r")]
-doc_list = [doc.strip('\n') for doc in open("doc_list.txt","r")]
+QueryList_index = enumerate(Query)
+DocList_index = enumerate(Doc)
 
-query_dataset = {}
-query_dataset_tf = {}
-doc_dataset = {}
-doc_dataset_tf = {}
+query_cnt = len(Query)
+doc_cnt = len(Doc)
 
-for query in query_list:
-	query_dataset.update({query:{}})
-	query_dataset_tf.update({query:{}})
-	word_cnt = 0
+Dictionary = {}
+DF = {}
+TF = {}
+QTF = {}
+
+for doc in Doc:
+	for line in open("Document/"+doc,"r").readlines()[3:]:
+		line = line.split()[0:-1]
+		for word in line:
+			if word not in Dictionary:
+				Dictionary.update({word:0})
+
+for query in Query:
 	for line in open("Query/"+query,"r"):
 		line = line.split()[0:-1]
 		for word in line:
-			if word in query_dataset[query]:
-				query_dataset[query].update({word:query_dataset[query][word]+1})
-			else:
-				query_dataset[query].update({word:1})
-			word_cnt += 1
-			
-	for word in query_dataset[query]:
-		query_dataset_tf[query].update({word:query_dataset[query][word]/word_cnt})
-				
-for doc in doc_list:
-	doc_dataset.update({doc:{}})
-	doc_dataset_tf.update({doc:{}})
-	f = open("Document/"+doc,"r").readlines()[3:]
-	word_cnt = 0
-	for line in f:
+			if word not in Dictionary:
+				Dictionary.update({word:0})
+
+for doc in Doc:
+	TF.update({doc:Dictionary.copy()})
+	for line in open("Document/"+doc,"r").readlines()[3:]:
 		line = line.split()[0:-1]
 		for word in line:
-			if word in doc_dataset[doc]:
-				doc_dataset[doc].update({word:doc_dataset[doc][word]+1})
-			else:
-				doc_dataset[doc].update({word:1})
-			word_cnt += 1
-			
-	for word in doc_dataset[doc]:
-		doc_dataset_tf[doc].update({word:doc_dataset[doc][word]/word_cnt})
+			TF[doc].update({word:TF[doc][word] + 1})
 
-doc_cnt = len(doc_dataset.keys())
-idf = {}
+for query in Query:
+	QTF.update({query:Dictionary.copy()})
+	for line in open("Query/"+query,"r"):
+		line = line.split()[0:-1]
+		for word in line:
+			QTF[query].update({word:QTF[query][word] + 1})
 
-for queries in query_list:
-	for words in query_dataset[queries].keys():
-		if words not in idf:
-			idf.update({words:get_idf(doc_dataset,words,doc_cnt)})
+for word in Dictionary.keys():
+	df_cnt = 0
+	for doc in Doc:
+		if TF[doc][word] > 0:
+			df_cnt += 1
+	DF.update({word:df_cnt})
 
 f = open("submission.txt", "w")
-f.write("Query,RetrievedDocuments\n")
-for queries in query_list:
-	f.write(queries+",")
-	query_tf_idf = {}
-	doc_tf_idf = {}
-	sim = {}
-	for words in query_dataset[queries].keys():
-		query_tf_idf.update({words:query_dataset_tf[queries][words] * idf[words]})
+f.write("Query,RetrievedDocuments\r\n")
 
-	for docs in doc_list:
-		doc_tf_idf.update({docs:{}})
-		for words in doc_dataset[docs]:
-			if words in idf:
-				doc_tf_idf[docs].update({words:doc_dataset_tf[docs][words] * idf[words]})
+for query in Query:
+	f.write(query+",")
+	QueryWeight = {}
+	DocWeight = {}
+	SIM = {}
+	for word in Dictionary.keys():
+		if DF[word] == 0:
+			QueryWeight.update({word:QTF[query][word] * math.log(doc_cnt)})
+		else:
+			QueryWeight.update({word:QTF[query][word] * math.log(doc_cnt/DF[word])})
+			
+	for doc in Doc:
+		DocWeight.update({doc:{}})
+		for word in Dictionary.keys():
+			if DF[word] == 0:
+				DocWeight[doc].update({word:TF[doc][word] * math.log(doc_cnt)})
 			else:
-				doc_tf_idf[docs].update({words:doc_dataset_tf[docs][words] * math.log(doc_cnt,10)})
-
-	for docs in doc_list:
+				DocWeight[doc].update({word:TF[doc][word] * math.log(doc_cnt/DF[word])})
+				
+	for doc in Doc:
 		s1 = 0
 		s2 = 0
 		s3 = 0
-		for words, q_tf_idf in query_tf_idf.items():
-			if words not in doc_tf_idf[docs]:
-				continue
-			else:
-				s1 += q_tf_idf * doc_tf_idf[docs][words]
-				s2 += math.pow(q_tf_idf,2)
-				s3 += math.pow(doc_tf_idf[docs][words],2)
+		for word, q_weight in QueryWeight.items():
+			s1 += q_weight * DocWeight[doc][word]
+			s2 += math.pow(q_weight,2)
+			s3 += math.pow(DocWeight[doc][word],2)
 		s2 = math.pow(s2,(1/2))
 		s3 = math.pow(s3,(1/2))
 		s4 = s2 * s3
-		if s4!=0:
-			sim.update({docs:s1/s4})
-		else:
-			sim.update({docs:0})
-	sim = sorted(sim.items(), key=lambda sim: sim[1],reverse=True)
-	for item in sim:
+		SIM.update({doc:s1/s4})
+		
+	SIM_SORT = sorted(SIM.items(), key=lambda SIM: SIM[1],reverse=True)
+	
+	for item in SIM_SORT:
 		f.write(item[0] + " ")
-	f.write("\n")
+	f.write("\r\n")
 
 f.close()
+			
